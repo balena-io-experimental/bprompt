7796f092b9be7d5ee4bb7d1a4c06d9fd08a107e9
Hugh Brown <hugh@balena.io>
2020-04-24 13:07:39 -0700

    Initial commit
    Signed-off-by: Hugh Brown <hugh@balena.io>

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+// A Small but Useful(tm) utility to manage different Balena profiles
+// and upate a Bash prompt accordingly.
+
+import (
+	"fmt"
+	// "os"
+	// "os/user"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+}
