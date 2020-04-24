0c5eeda1a8f6de6be383c597c5bb675a3767588c
Hugh Brown <hugh@balena.io>
2020-04-24 13:21:37 -0700

    Start adding accounts
    Signed-off-by: Hugh Brown <hugh@balena.io>

--- main.go
+++ main.go
@@ -4,11 +4,56 @@ package main
 // and upate a Bash prompt accordingly.
 
 import (
+	"flag"
 	"fmt"
 	// "os"
 	// "os/user"
 )
 
+type balenaAccount struct {
+	Name      string
+	Url       string
+	TokenName string
+	Emoji     string
+}
+
+// FIXME: Hardcoding for now
+
+var myAccounts = []balenaAccount{
+	balenaAccount{
+		Name:      "support",
+		Url:       "balena-cloud.com",
+		TokenName: "support",
+		Emoji:     "🔥⚠😑",
+	},
+	balenaAccount{
+		Name:      "personal",
+		Url:       "balena-cloud.com",
+		TokenName: "personal",
+		Emoji:     "🐩",
+	},
+	balenaAccount{
+		Name:      "staging",
+		Url:       "balena-staging.com",
+		TokenName: "staging",
+		Emoji:     "🐋",
+	},
+	balenaAccount{
+		Name:      "local",
+		Url:       "my.devenv.local",
+		TokenName: "local",
+		Emoji:     "⛳",
+	},
+}
+
 func main() {
 	fmt.Println("Hello, world!")
+	printPtr := flag.Bool("print", false, "print accounts")
+	flag.Parse()
+	if *printPtr == true {
+		fmt.Println("List of accounts")
+		for _, acct := range myAccounts {
+			fmt.Printf("%s %s \n", acct.Name, acct.Emoji)
+		}
+	}
 }
